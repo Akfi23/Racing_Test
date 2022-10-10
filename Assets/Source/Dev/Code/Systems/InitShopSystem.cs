2cca1079a4644032fff531c3d9851e9ec1a3e53f
Andrey <708.akfi@gmail.com>
2022-10-10 17:44:24 +0300

    [Add] Addressables load

--- Assets/Source/Dev/Code/Systems/InitShopSystem.cs
+++ Assets/Source/Dev/Code/Systems/InitShopSystem.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class InitShopSystem : GameSystemWithScreen<ShopScreen> // Rename to ColorSelectorSystem !!!
 {
-    [SerializeField] private ColorButtonComponent _buttonPrefab;
+    [SerializeField] private AssetReference _buttonPrefab;
+    [SerializeField] private AssetReference[] _colorConfigsAsset;
 
     private Dictionary<string, ColorConfig> _colorConfigs = new Dictionary<string, ColorConfig>();
 
-    public override void OnInit()
+    public async override void OnInit()
     {
         screen.ShopButton.onClick.AddListener(ToggleShopWindow);
 
-        LoadColorConfigsToDictionary();
-        InitColorPanel();
+        for (int i = 0; i < _colorConfigsAsset.Length; i++)
+        {
+            await LoadColorConfig(i);
+        }
+
+        await LoadButtonPrefab();
+
         SetColorBySave(player.ColorName);
     }
 
@@ -23,22 +33,31 @@ public class InitShopSystem : GameSystemWithScreen<ShopScreen> // Rename to Colo
         screen.ShopWindow.gameObject.SetActive(!screen.ShopWindow.gameObject.activeSelf);
     }
 
-    private void LoadColorConfigsToDictionary()
+    private async UniTask LoadColorConfig(int configIndex)
     {
-        _colorConfigs = Resources.LoadAll<ColorConfig>("ColorConfigs").ToDictionary(x=>x.Name);
-    }
+        AsyncOperationHandle<ColorConfig> handle = _colorConfigsAsset[configIndex].LoadAssetAsync<ColorConfig>();
+        await handle.ToUniTask();
 
-    private void InitColorPanel()
-    {
-        foreach (var config in _colorConfigs)
+        if (handle.Status == AsyncOperationStatus.Succeeded)
         {
-            var newColorButton = Instantiate(_buttonPrefab, screen.ContentParent);
-            newColorButton.InitButton(config.Value.Color);
-            newColorButton.Button.onClick.AddListener(() => SetCarColor(config.Value.ColorMaterial,config.Value.Name));
+            ColorConfig config = handle.Result;
+            _colorConfigs.Add(config.Name, config);
+            Addressables.Release(handle);
         }
     }
 
-    private void SetCarColor(Material material,string name)
+    private async UniTask LoadButtonPrefab()
+    {
+        foreach (var config in _colorConfigs)
+        {
+            var button = await _buttonPrefab.InstantiateAsync(screen.ContentParent).ToUniTask();
+            button.TryGetComponent(out ColorButtonComponent colorButton);
+            colorButton.InitButton(config.Value.Color);
+            colorButton.Button.onClick.AddListener(() => SetCarColor(config.Value.ColorMaterial, config.Value.Name));
+        }
+    }
+
+    private void SetCarColor(Material material, string name)
     {
         game.Player.BodyRenderer.material = material;
         player.ColorName = name;
@@ -49,6 +68,6 @@ public class InitShopSystem : GameSystemWithScreen<ShopScreen> // Rename to Colo
         var material = _colorConfigs[name].ColorMaterial;
         var color = _colorConfigs[name].Name;
 
-        SetCarColor(material,color);
+        SetCarColor(material, color);
     }
 }
--- Assets/Source/Dev/Code/Systems/StartRaceSystem.cs
+++ Assets/Source/Dev/Code/Systems/StartRaceSystem.cs
@@ -4,31 +4,48 @@ using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Linq;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.SceneManagement;
 
 public class StartRaceSystem : GameSystemWithScreen<MenuScreen>
 {
-    [SerializeField] private LevelButtonComponent _buttonPrefab;
+    [SerializeField] private AssetReference _buttonPrefab;
+    [SerializeField] private AssetReference[] _levelConfigAssets;
+    [SerializeField] private AssetReference[] _scenes;
+
     private LoadingScreen _loadingScreen;
 
-    public override void OnInit()
+    public async override void OnInit()
     {
         _loadingScreen = UIManager.GetUIScreen<LoadingScreen>();
         screen.StartRaceButton.onClick.AddListener(ToggleLevelWindow);
 
-        InitLevelButtons();
+        for (int i = 0; i < _levelConfigAssets.Length; i++)
+        {
+            await InitLevelConfigs(i);
+        }
     }
 
-    private void InitLevelButtons()
+    private async UniTask InitLevelConfigs(int configIndex)
     {
-        var levelConfigs = Resources.LoadAll<LevelConfig>("LevelConfigs");
+        AsyncOperationHandle<LevelConfig> levelHandle = _levelConfigAssets[configIndex].LoadAssetAsync<LevelConfig>();
+        await levelHandle.ToUniTask();
 
-        foreach (var config in levelConfigs)
+        if (levelHandle.Status == AsyncOperationStatus.Succeeded)
         {
-            var button = Instantiate(_buttonPrefab, screen.LevelSelectorWindow);
-            button.InitButton(config.LevelNumber);
-            button.LevelButton.OnClickAsAsyncEnumerable().Subscribe(a => LoadLevelAsync(config.LevelNumber));
+            LevelConfig config = levelHandle.Result;
+            await InitLevelButton(config);
+            Addressables.Release(levelHandle);
         }
+    }
+
+    private async UniTask InitLevelButton(LevelConfig config)
+    {
+        var button = await _buttonPrefab.InstantiateAsync(screen.LevelSelectorWindow).ToUniTask();
+        button.TryGetComponent(out LevelButtonComponent levelButton);
+        levelButton.InitButton(config.LevelNumber);
+        levelButton.LevelButton.OnClickAsAsyncEnumerable().Subscribe(a => LoadLevelAsync(config.LevelNumber));
     }
 
     private void ToggleLevelWindow()
@@ -47,7 +64,7 @@ public class StartRaceSystem : GameSystemWithScreen<MenuScreen>
         _loadingScreen.LoadingImage.transform.rotation = Quaternion.Euler(Vector3.zero);
 
         _loadingScreen.BackgroundImage.DOFade(1,0.15f).SetEase(Ease.Linear)
-            .OnComplete(async ()=> await SceneManager.LoadSceneAsync("Level" + levelNumber, LoadSceneMode.Additive).ToUniTask().ToCoroutine());
+            .OnComplete(async ()=> await _scenes[levelNumber-1].LoadSceneAsync(LoadSceneMode.Additive).ToUniTask());
 
         _loadingScreen.LoadingImage.transform.DORotate(Vector3.up * 90f, 0.3f).SetLoops(5, LoopType.Incremental).SetEase(Ease.Linear).
             OnComplete(() => _loadingScreen.BackgroundImage.DOFade(0, 0.3f));
